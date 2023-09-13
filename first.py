5f1ceecedc52b2736c88b20680d94b22c2470006
amirhussain.en <61998867+amircomputerinc@users.noreply.github.com>
2023-09-13 10:48:56 +0430

    create class car
    add car class name : bmw ,benz,sipa

--- first.py
+++ first.py
@@ -1,8 +1,25 @@
-print("hi")
-# first line
+class car:
+    pass
 
-x=2
-y=3
-z=y+x
+car1=car()
+car2=car()
+car3=car()
 
-# asgind x and y
+car1.name="benz"
+car1.model="s500"
+car1.coler="black"
+car1.motor="petrol"
+#----------------------------------------------------------------
+#crate object bmw 
+car2.name="BMW"
+car2.model="Z4"
+car2.coler="orang"
+car2.motor="petrol"
+#------------------------------------------------------------------
+car3.name="sipa"
+car3.model="pride"
+car3.coler="wh"
+car3.motor="petrol"
+#-------------------------------------------------------------------
+
+print(car1.name,car2.name,car3.name)
